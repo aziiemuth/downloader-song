6817a45481e0fa98dad4ae6f91d36062f52465a3
Athiief <aziiemuth@gmail.com>
2026-06-21 19:01:54 +0700

    Update player_clients, add user-agent headers, and add .gitignore

--- main.py
+++ main.py
@@ -89,7 +89,13 @@ def download(query, out="downloads"):
         "logger": QuietLogger(),
 
         "extractor_args": {
-            "youtube": {"player_client": ["web_safari"]}
+            "youtube": {
+                "player_client": ["android", "web"],
+                "skip": ["webpage", "dash", "hls"]
+            }
+        },
+        "http_headers": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
         },
 
         "progress_hooks": [progress_hook],
